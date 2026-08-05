374f78acd6a770418cf147a49f56740817e28c1a
melderis-fbs <admin@foundersbs.com>
2026-08-05 20:24:23 -0300

    Updated index.ts

--- supabase/functions/synoma-chat/index.ts
+++ supabase/functions/synoma-chat/index.ts
@@ -17,7 +17,7 @@ const MAX_PEDIDOS_HORA = 5;
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
 
